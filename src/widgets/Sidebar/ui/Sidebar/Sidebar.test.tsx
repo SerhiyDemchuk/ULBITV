cd993e7cf088894ee4996a7f40bf691377048279
SergDemch <sergeylobster@gmail.com>
2022-10-01 23:54:36 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,19 +1,12 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar'
+import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import {
-  renderWithTranslation
+  renderWithTranslation,
 } from 'shared/lib/classNames/tests/renderWithTranslation/renderWithTranslation';
 
 describe('Sidebar', () => {
   test('with only first param', () => {
     renderWithTranslation(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
-  });
-  test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
-    const toggleBtn = screen.getByTestId('sidebar-toggle');
-    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
-    fireEvent.click(toggleBtn);
-    expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
   });
 });
