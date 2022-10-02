1a211c1cc553ed881cab05aa738db461aeecead7
SergDemch <sergeylobster@gmail.com>
2022-10-02 16:50:34 +0300

    l28. fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -3,10 +3,18 @@ import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import {
   renderWithTranslation,
 } from 'shared/lib/classNames/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/classNames/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+  });
+  test('test toggle', () => {
+    componentRender(<Sidebar />);
+    const toggleBtn = screen.getByTestId('sidebar-toggle');
+    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+    fireEvent.click(toggleBtn);
+    expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
   });
 });
