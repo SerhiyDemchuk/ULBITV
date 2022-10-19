ae9e41e02e4ae3cf1ec8dcee9e6ee2f92b7822cf
SergDemch <sergeylobster@gmail.com>
2022-10-19 17:07:16 +0300

    l36. AuthByUsername tests. TestAsyncThunk.ts

---
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { getLoginError } from './getLoginError';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+describe('getLoginError.test', () => {
+  test('should return error', () => {
+    const state: DeepPartial<StateSchema> = {
+      loginForm: {
+        error: 'error',
+      },
+    };
+    expect(getLoginError(state as StateSchema)).toBe('error');
+  });
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginError(state as StateSchema)).toBe(undefined);
+  });
+});
